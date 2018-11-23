0ea5516b1f5c452ca275b308ea8dbd47848dcced
wenjing-liu <wenjing.liu09@foxmail.com>
2018-11-23 16:54:40 +0800

    1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n1. init App 2. add vue-i18n

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SimpleSpreadsheet :msg='msg'/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SimpleSpreadsheet from '@/components/SimpleSpreadsheet.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SimpleSpreadsheet
+  },
+  data () {
+    return {
+      msg: 'Welcome to Simple SpreadSheet'
+    }
   }
 }
 </script>
